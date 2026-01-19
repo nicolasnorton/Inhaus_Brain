eaea957d37ea8da5de25a71fbb6a392df8ebedd6
Nicolas Norton <nicolasnorton@gmail.com>
2026-01-19 03:05:58 -0500

    Remove unused dart:io import from AppVideoPlayer

--- lib/core/widgets/app_video_player.dart
+++ lib/core/widgets/app_video_player.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'dart:io';
 import 'package:flutter/foundation.dart'; // For kIsWeb
 
 class AppVideoPlayer extends StatefulWidget {
