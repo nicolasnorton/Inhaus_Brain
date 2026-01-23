8c2a1a3f57382d5c865d720d0003d70c55861096
Nicolas Norton <nicolasnorton@gmail.com>
2026-01-23 12:57:52 -0500

    Add missing dart:convert import

--- lib/features/assistant/widgets/ai_assistant_overlay.dart
+++ lib/features/assistant/widgets/ai_assistant_overlay.dart
@@ -1,5 +1,6 @@
 // import 'dart:io' show File, Directory; // Removed to avoid web crashes 
 
+import 'dart:convert';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
